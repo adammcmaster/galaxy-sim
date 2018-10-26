249c59e626b3fad12e166aaf9f35723e36dea0ed
Adam McMaster <adam@zooniverse.org>
2018-10-26 13:03:21 +0100

    Saving and reloading data

--- clump_find.py
+++ clump_find.py
@@ -1,67 +1,65 @@
+import os
 import sys
+
+sys.path.insert(0, '/mnt/zfsusers/mcmaster/.virtualenvs/clumps/lib/python2.7/site-packages')
 
 import yt
 from yt.analysis_modules.level_sets.api import *
 
-from amr2cube import amr2cube
+from ramses import RamsesData
 
-class RamsesData:
 
-    def __init__(self,idir=1,type=1,xmin=0,xmax=1,ymin=0,ymax=1,zmin=0,zmax=1,lmax=5):
-        """ Read RAMSES data and return a cube """
+GALAXY_CENTRE = [0.706731, 0.333133, 0.339857]
+CUBE_PADDING = 0.001
+MAX_LEVEL = int(sys.argv[1])
 
-        #Read data in file
-        dirname='output_'+str_suffix(idir)
+DATA_PATH = 'data'
+RAMSES_INPUT_DIR = os.path.join(DATA_PATH, 'output_00149')
+CUBE_DIR = os.path.join(DATA_PATH, 'cubes')
+PLOT_DIR = os.path.join(DATA_PATH, 'plots')
+CLUMP_DIR = os.path.join(DATA_PATH, 'clumps')
 
-        #Cube size parameter
-        n=2**lmax
-        nx=int((xmax-xmin)*n)
-        ny=int((ymax-ymin)*n)
-        nz=int((zmax-zmin)*n)
-
-        self.xmin=xmin ; self.xmax=xmax
-        self.ymin=ymin ; self.ymax=ymax
-        self.zmin=zmin ; self.zmax=zmax
-
-        self.cube,self.time=amr2cube(dirname,type,xmin,xmax,\
-                                       ymin,ymax,zmin,zmax,lmax,nx,ny,nz)
-
-def str_suffix(n,length=5):
-    fewzero=''
-    for i in range(length-len(str(n))):
-        fewzero=fewzero+'0'
-    return fewzero+str(n)
-
+if not os.path.exists(CUBE_DIR):
+    os.makedirs(CUBE_DIR)
+if not os.path.exists(PLOT_DIR):
+    os.makedirs(PLOT_DIR)
+if not os.path.exists(CLUMP_DIR):
+    os.makedirs(CLUMP_DIR)
 
 cube_data = RamsesData(
-    idir=149,
-    type=1,
-    xmin=0.700731,
-    xmax=0.710731,
-    ymin=0.330133,
-    ymax=0.340133,
-    zmin=0.330857,
-    zmax=0.350857,
-    lmax=16,
+    idir=RAMSES_INPUT_DIR,
+    xmin=GALAXY_CENTRE[0] - CUBE_PADDING,
+    xmax=GALAXY_CENTRE[0] + CUBE_PADDING,
+    ymin=GALAXY_CENTRE[1] - CUBE_PADDING,
+    ymax=GALAXY_CENTRE[1] + CUBE_PADDING,
+    zmin=GALAXY_CENTRE[2] - CUBE_PADDING,
+    zmax=GALAXY_CENTRE[2] + CUBE_PADDING,
+    lmax=MAX_LEVEL,
+    save_dir=CUBE_DIR,
 )
 
 ds = yt.load_uniform_grid(dict(density=cube_data.cube), cube_data.cube.shape)
 data_source = ds.disk(
-    [0.706731, 0.333133, 0.339857],
+    GALAXY_CENTRE,
     [0., 0., 1.],
     (1, 'kpc'),
     (0.5, 'kpc'),
 )
 
-master_clump = Clump(data_source, "density")
-
 c_min = data_source["density"].min()
 c_max = data_source["density"].max()
 step = 8.0
-find_clumps(master_clump, c_min, c_max, step)
+
+clump_file = os.path.join(CLUMP_DIR, '{}_clumps.h5'.format(MAX_LEVEL))
+if os.path.isfile(clump_file):
+    master_clump = yt.load(clump_file)
+else:
+    master_clump = Clump(data_source, "density")
+    find_clumps(master_clump, c_min, c_max, step)
+    master_clump.save_as_dataset(clump_file, ['density'])
 
 leaf_clumps = get_lowest_clumps(master_clump)
 
-plot = yt.ProjectionPlot(ds, "x", "density", center=[0.706731, 0.333133, 0.339857], width=(2, 'kpc'))
+plot = yt.ProjectionPlot(ds, "x", "density", center=GALAXY_CENTRE)
 plot.annotate_clumps(leaf_clumps)
-plot.save('clumps')
+plot.save(os.path.join(PLOT_DIR, 'clumps{}'.format(MAX_LEVEL)))
---
+++ ramses.py
@@ -0,0 +1,54 @@
+import os
+
+import numpy
+
+from amr2cube import amr2cube
+
+
+class RamsesData:
+    def __init__(
+        self,
+        idir,
+        type=1,
+        xmin=0,
+        xmax=1,
+        ymin=0,
+        ymax=1,
+        zmin=0,
+        zmax=1,
+        lmax=5,
+        save_dir=None,
+    ):
+        """ Read RAMSES data and return a cube """
+
+        cube_path = os.path.join(save_dir, '{}_cube.npy'.format(lmax))
+        time_path = os.path.join(save_dir, '{}_time.txt'.format(lmax))
+
+        if (
+            save_dir 
+            and os.path.isfile(cube_path)
+            and os.path.isfile(time_path)
+        ):
+            self.cube = numpy.load(cube_path)
+            with open(time_path) as time_file:
+                self.time = float(time_file.read().strip())
+
+            return
+
+        #Cube size parameter
+        n=2**lmax
+        nx=int((xmax-xmin)*n)
+        ny=int((ymax-ymin)*n)
+        nz=int((zmax-zmin)*n)
+
+        self.xmin=xmin ; self.xmax=xmax
+        self.ymin=ymin ; self.ymax=ymax
+        self.zmin=zmin ; self.zmax=zmax
+
+        self.cube,self.time=amr2cube(idir,type,xmin,xmax,\
+                                       ymin,ymax,zmin,zmax,lmax,nx,ny,nz)
+
+        if save_dir:
+            numpy.save(cube_path, self.cube)
+            with open(time_path, 'w') as time_file:
+                time_file.write(str(self.time))
